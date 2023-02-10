fec5dba12865ba5ba9dc55ee787c7332c38f60b9
Mani7Sai <112700177+Mani7Sai@users.noreply.github.com>
2023-02-10 14:32:02 +0530

    Update 02. String to Integer.java

--- 02. String to Integer.java
+++ 02. String to Integer.java
@@ -3,11 +3,19 @@ class strtoint
 {
 public static void main(String [] args)
 {
+try
+{
 String a;
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter:");
+System.out.println("Enter a value:");
 a=sc.nextLine();
 int b=Integer.parseInt(a);
+System.out.println("The integer value given is:");
 System.out.println(+b);
+}
+catch(Exception e)
+{
+System.out.println("Invalid! It can't be changed into Integer");
+}
 }
 }
