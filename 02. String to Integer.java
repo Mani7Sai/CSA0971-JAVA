79cfcf8a09e9acd711192dd97ecbf122e093581c
Mani7Sai <112700177+Mani7Sai@users.noreply.github.com>
2023-02-10 14:20:14 +0530

    Create 02. String to Integer.java

---
+++ 02. String to Integer.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class strtoint
+{
+public static void main(String [] args)
+{
+String a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter:");
+a=sc.nextLine();
+int b=Integer.parseInt(a);
+System.out.println(+b);
+}
+}
